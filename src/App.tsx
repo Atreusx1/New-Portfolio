19e504fb4268e00f418415b129a352398f486101
Atreusx1 <anishkadam92@gmail.com>
2026-08-05 17:58:07 +0530

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -105,8 +105,8 @@ const App = () => {
                 color: "var(--fg-muted)",
               }}
             >
-              © {new Date().getFullYear()} ANISH KADAM · BUILT WITH ❤️ USING
-              REACT, THREE.JS, AND CREATIVITY
+              © {new Date().getFullYear()} ANISH KADAM · BUILT WITH {"\u2764"}{" "}
+              USING REACT, THREE.JS, AND CREATIVITY
             </span>
           </footer>
         </div>
